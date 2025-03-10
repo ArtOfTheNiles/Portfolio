42380002804be290a11845ff81a3719ad41869bf
Niles Bontrager <artoftheniles@yahoo.com>
2025-03-09 21:30:09 -0400

    Adding Skill Types as I need more

--- server/src/models/skillTypes.ts
+++ server/src/models/skillTypes.ts
@@ -1,6 +1,10 @@
 export enum SkillType {
   DigitalArt = 'Digital Art',
+  Film = 'Film',
+  Compositing = 'Compositing',
+  MotionGraphics = 'Motion Graphics',
   TraditionalArt = 'Traditional Art',
+  TraditionalAnimation = 'Traditional Animation',
   Modeling = '3D Modeling',
   Animation = 'Animation',
   GraphicDesign = 'Graphic Design',

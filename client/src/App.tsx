5f3df21dfe8f85d840c269071d7401da5652e160
Niles Bontrager <nbontrager@porterone.local>
2025-03-10 12:21:17 -0400

    Removed false header and footer. Oh rookie choices

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,8 +5,6 @@ import { Projects } from "./components/Projects.tsx"
 import { Resume } from "./components/Resume.tsx"
 import { Skills } from "./components/Skills.tsx"
 import { Footer } from "./components/Footer.tsx"
-import { FalseHeader } from "./components/FalseHeader.tsx"
-import { FalseFooter } from "./components/falseFooter.tsx"
 
 
 //Layout of the website
@@ -14,13 +12,11 @@ function App() {
   return (
     <>
       <Header />
-      <FalseHeader />
       <Projects />
       <Skills />
       <Resume />
       <About />
       <Contact />
-      <FalseFooter />
       <Footer />
     </>
   )
--- client/src/components/falseFooter.tsx
+++
@@ -1,7 +0,0 @@
-import '@/styles/falseFooter.css'
-// Leave room at the bottom of the page for the footer
-export const FalseFooter = () => {
-    return (
-        <div id="false-footer"> </div>
-    )
-}

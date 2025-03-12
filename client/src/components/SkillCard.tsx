5d0819f337a904bcb1d08d50ae518cef186eb3f9
Niles Bontrager <artoftheniles@yahoo.com>
2025-03-12 13:15:58 -0400

    SkillCard styling, removed unused selectors

--- client/src/components/SkillCard.tsx
+++ client/src/components/SkillCard.tsx
@@ -1,38 +1,49 @@
 import { useState } from "react"
 
 import { SkillProps } from "../interfaces/skill.interface"
-import '@/styles/skillCard.css'
+import "@/styles/skillCard.css"
 
 export const SkillCard = (props: SkillProps) => {
   const DEFAULT_VIEW_LIMIT = 2;
   const [viewLimit, setViewLimit] = useState(DEFAULT_VIEW_LIMIT);
 
+  const image = props.iconURL ? props.iconURL : "https://picsum.photos/64";
   const displayedSkillTypes = props.skillType.slice(0, viewLimit);
   const hasMoreSkillTypes = viewLimit < props.skillType.length;
 
   return (
     <div className="skill-card">
-      <h3 className="skill-title">{props.skillName}</h3>
-      <ul className="skill-types">
-        {displayedSkillTypes.map((_, index) => (
-            <li className="skill-type">{props.skillType[index]}</li>
-        ))}
-        {hasMoreSkillTypes && (
-        <button 
-            className="show-more"
-            onClick={() => setViewLimit((prev) => prev + DEFAULT_VIEW_LIMIT)}
-        >
-            More ...
-        </button>
-        )}
-      </ul>
-      <p className="skill-description">{props.description}</p>
+      <div className="skill-header">
+        <img className="skill-icon" src={image} alt={props.skillName + " Icon"} />
+        <h3 className="skill-title"><a href={props.skillURL}>{props.skillName}</a></h3>
+        <small>
+          <ul className="skill-types">
+            {displayedSkillTypes.map((_, index) => (
+                <li className="skill-type">{props.skillType[index]}</li>
+            ))}
+            {hasMoreSkillTypes && (
+            <button 
+                className="show-more"
+                onClick={() => setViewLimit((prev) => prev + DEFAULT_VIEW_LIMIT)}
+            >
+                More ...
+            </button>
+            )}
+          </ul>
+        </small>
+      </div>
+      <div className="skill-body">
+
+      </div>
+
+      <p className="summary">{props.summary}</p>
+      <p className="summary tooltiptext">{props.description}</p>
       <ul className="skill-levels">
-        <li className="tooltip">Confidence: {props.confidenceLevel}
-            <p className="tooltiptext">{props.confidenceDescription}</p>
+        <li className="confidence tooltip">Confidence: {props.confidenceLevel}
+            <p className="confidence tooltiptext">{props.confidenceDescription}</p>
         </li>
-        <li className="tooltip">Passion: {props.passionLevel}
-            <p className="tooltiptext">{props.confidenceDescription}</p>
+        <li className="passion tooltip">Passion: {props.passionLevel}
+            <p className="passion tooltiptext">{props.confidenceDescription}</p>
         </li>
       </ul>
     </div>
